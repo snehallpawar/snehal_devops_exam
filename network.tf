82d540b12560fc64ef1ff0d77328290bbdcfb664
snehallpawar <100402255+snehallpawar@users.noreply.github.com>
2025-01-31 13:41:50 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "private_subnet" {
   vpc_id     = "vpc-06b326e20d7db55f9"
-  cidr_block = "10.0.11.0/24"  # Use an available block, e.g., 10.0.X.0/24
+  cidr_block = "10.0.61.0/24"  # Use an available block, e.g., 10.0.X.0/24
   availability_zone = "ap-south-1a"  # Choose an AZ
   map_public_ip_on_launch = false  # Since it’s a private subnet
 
