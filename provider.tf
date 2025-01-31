c23006fa40c0fd224db833ecd863fcc9ed83b47d
snehallpawar <100402255+snehallpawar@users.noreply.github.com>
2025-01-31 11:48:27 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,3 @@
-provider "aws" {
-  region = "ap-south-1"
-}
 terraform {
   backend "s3" {
     bucket = "467.devops.candidate.exam"
@@ -9,5 +6,7 @@ terraform {
   }
 }
 
-
+provider "aws" {
+  region = "ap-south-1"
+}
 # Add your S3 backend configuration here
