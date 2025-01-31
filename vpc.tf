aaff3cd278c6e94735d9d78c0153f94dbe0dea7e
snehallpawar <100402255+snehallpawar@users.noreply.github.com>
2025-01-31 12:14:02 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = "10.0.0.0/16"
-  cidr_block              = "10.0.1.252/24"
+  cidr_block              = "10.0.4.0/24"
   availability_zone       = "ap-south-1a" # Modify based on availability zone
   map_public_ip_on_launch = false
   tags = {
