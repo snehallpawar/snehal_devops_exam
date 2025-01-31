6914953200732cf793d084d78b7bb37d6eb732aa
snehallpawar <100402255+snehallpawar@users.noreply.github.com>
2025-01-31 12:34:41 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = "vpc-06b326e20d7db55f9"  # Replace with your VPC ID
-  cidr_block              = "10.0.0.0/16"  # Replace with an available CIDR block
+  cidr_block              = "10.0.4.0/24"  # Replace with an available CIDR block
   availability_zone       = "ap-south-1a"  # Choose the availability zone
   map_public_ip_on_launch = false
   tags = {
@@ -13,7 +13,7 @@ resource "aws_route_table" "private_route_table" {
   vpc_id = "vpc-06b326e20d7db55f9"  # Replace with your VPC ID
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = "10.0.0.0/16"
     gateway_id = "nat-0a34a8efd5e420945"  # Replace with your NAT Gateway ID
   }
 
