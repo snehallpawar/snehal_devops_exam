4672dde34964e603bfd97e9e2daf8452df141897
snehallpawar <100402255+snehallpawar@users.noreply.github.com>
2025-01-31 12:12:50 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,25 +1,9 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
 resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.4.0/24"  # Choose any available CIDR block
-  availability_zone       = "ap-south-1"   # Use the preferred AZ
+  vpc_id                  = "10.0.0.0/16"
+  cidr_block              = "10.0.1.252/24"
+  availability_zone       = "ap-south-1a" # Modify based on availability zone
   map_public_ip_on_launch = false
-}
-
-resource "aws_security_group" "lambda_sg" {
-  name        = "lambda_security_group"
-  description = "Security group for Lambda"
-  vpc_id      = aws_vpc.main.id
-}
-
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table_association" "private_route_association" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_route_table.id
+  tags = {
+    Name = "private-subnet"
+  }
 }
