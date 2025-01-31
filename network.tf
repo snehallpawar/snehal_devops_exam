f7f0670723f76d9d4b4c6e62363172ac298b6f22
snehallpawar <100402255+snehallpawar@users.noreply.github.com>
2025-01-31 13:25:06 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,80 +1,77 @@
-resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "main-vpc"
+terraform {
+  backend "s3" {
+    bucket = "467.devops.candidate.exam"
+    region = "ap-south-1"
+    key    = "<Your First Name>.<Your Last Name>"
   }
 }
 
-# Define the Private Subnet
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Data sources for existing resources
+data "aws_nat_gateway" "nat" {
+  id = "nat-0a34a8efd5e420945"
+}
+
+data "aws_vpc" "vpc" {
+  id = "vpc-06b326e20d7db55f9"
+}
+
+data "aws_iam_role" "lambda" {
+  name = "DevOps-Candidate-Lambda-Role"
+}
+
+# Private Subnet
 resource "aws_subnet" "private_subnet" {
-  vpc_id                  = vpc-06b326e20d7db55f9
-  cidr_block              = "10.0.1.0/24"  # Replace with an available CIDR block
-  availability_zone       = "ap-south-1a"  # Choose the availability zone
+  vpc_id                  = data.aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"  # Choose your desired CIDR
+  availability_zone       = "ap-south-1a"  # You can choose the availability zone
   map_public_ip_on_launch = false
-  tags = {
-    Name = "private-subnet"
-  }
 }
 
-# Define the NAT Gateway (this will allow outbound traffic for the private subnet)
-resource "aws_nat_gateway" "nat_gateway" {
-  allocation_id = aws_eip.nat_ip.id
-  subnet_id     = aws_subnet.private_subnet.id
-
-  tags = {
-    Name = "NAT-Gateway"
-  }
-}
-
-# Define the Elastic IP for the NAT Gateway
-resource "aws_eip" "nat_ip" {
-  vpc = true
-}
-
-# Define the Route Table for the Private Subnet
+# Routing Table
 resource "aws_route_table" "private_route_table" {
-  vpc_id = vpc-06b326e20d7db55f9
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = nat-0a34a8efd5e420945  # Use the NAT Gateway ID
-  }
-
-  tags = {
-    Name = "private-route-table"
-  }
+  vpc_id = data.aws_vpc.vpc.id
 }
 
-# Associate the private route table with the private subnet
-resource "aws_route_table_association" "private_route_table_association" {
+resource "aws_route_table_association" "private_subnet_association" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_route_table.id
 }
 
-# Define Security Group for the Lambda function inside VPC
-resource "aws_security_group" "lambda_sg" {
-  name        = "DevOps-Candidate-Lambda-Role"
-  description = "Security group for Lambda function"
-  vpc_id      = aws_vpc.main_vpc.id
+# Lambda Security Group
+resource "aws_security_group" "lambda_security_group" {
+  vpc_id = data.aws_vpc.vpc.id
+  name   = "lambda_sg"
 
   ingress {
-    from_port   = 0
-    to_port     = 65535
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# Output the Private Subnet ID (This will be used dynamically in Lambda configuration)
-output "private_subnet_id" {
-  value = aws_subnet.private_subnet.id
+# Lambda Function
+resource "aws_lambda_function" "lambda" {
+  function_name = "devops_lambda_function"
+  role          = data.aws_iam_role.lambda.arn
+  handler       = "index.handler"
+  runtime       = "nodejs14.x"  # Choose appropriate runtime for your Lambda function
+  filename      = "lambda_function.zip"  # Ensure you have zipped your Lambda code
+  source_code_hash = filebase64sha256("lambda_function.zip")
+  
+  vpc_config {
+    subnet_ids         = [aws_subnet.private_subnet.id]
+    security_group_ids = [aws_security_group.lambda_security_group.id]
+  }
 }
