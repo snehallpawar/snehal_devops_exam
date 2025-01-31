61a80ae968d30fd5425f6c66c1ca2fdadb8de6e6
snehallpawar <100402255+snehallpawar@users.noreply.github.com>
2025-01-31 12:26:20 +0530

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,23 @@
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = "vpc-06b326e20d7db55f9"  # Replace with your VPC ID
+  cidr_block              = "10.0.1.0/24"  # Replace with an available CIDR block
+  availability_zone       = "ap-south-1a"  # Choose the availability zone
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+# Define Route Table for the Private Subnet
+resource "aws_route_table" "private_route_table" {
+  vpc_id = "vpc-06b326e20d7db55f9"  # Replace with your VPC ID
+
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "nat-0a34a8efd5e420945"  # Replace with your NAT Gateway ID
+  }
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
